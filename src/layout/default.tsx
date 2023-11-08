244e57a268ab5f35cf8d5b7d0b00947045dbe960
Dhika Rizky <deka_r@outlook.com>
2023-11-08 21:43:15 +0700

    Add homepage layout

--- src/layout/default.tsx
+++ src/layout/default.tsx
@@ -15,8 +15,12 @@ const Default: FC<Props> = ({ children, title }) => {
         <title>{title ? `${title} – Rungkadism` : 'Rungkadism'}</title>
         <meta charset="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+        <link href="https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:ital,wght@0,400;0,700;1,500&display=swap" rel="stylesheet" />
+        <link rel="stylesheet" href={Netlify.env.get("DENO_DEPLOYMENT_ID") ? '/static/output.css' : 'http://localhost:3999/static/output.css'} />
       </head>
-      <body>
+      <body class="text-[#2d2d2d] dark:text-white dark:bg-[#2d2d2d] font-['Plus_Jakarta_Sans']">
         { children }
       </body>
     </html>
--- src/views/index.tsx
+++ src/views/index.tsx
@@ -9,7 +9,16 @@ import Layout from '../layout/default.tsx'
 const Index: FC = () => {
   return (
     <Layout>
-      Rungkadism
+      <nav class="p-8">
+        <h1 class="font-bold text-[#e31206] dark:text-[#ff685f] text-2xl text-center">Rungkadism</h1>
+      </nav>
+      <div class="mt-16 flex justify-center flex-col">
+        <span class="text-[#e31206] dark:text-[#ff685f] sm:text-8xl lg:text-9xl text-center font-bold">0 Jam</span>
+        <span class="text-2xl text-center font-medium mt-4">Sejak masalah terakhir LRT Jabodebek</span>
+        <span class="text-xl text-center font-medium mt-2">(berdasarkan laporan <a href="https://google.com" class="underline">berikut</a>)</span>
+
+        <span class="text-lg text-center font-medium mt-12">Ke-<i class="italic">rungkad</i>-an baru? Laporan sebelumnya salah? <a href="/lapor" class="underline">Buat laporan baru</a></span>
+      </div>
     </Layout>
   )
 }
